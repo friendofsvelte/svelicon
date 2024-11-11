4dbbbd72dc9343cc62e74b540e7a2ee4eef495e3
Bishwas Bhandari <bishwasbh@gmail.com>
2024-11-11 11:06:46 +0545

    downloder optz

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ import {parseSVGContent, convertParsedSVG, iconToSVG} from '@iconify/utils';
 const capitalizeFirstLetter = string => string.charAt(0).toUpperCase() + string.slice(1);
 
 // Common template parts
-const generateSvgTemplate = (pathData, width, height, size) => `
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="{${size}}em"
-    height="{${size}}em"
-    viewBox="0 0 ${width} ${height}">
-    ${pathData}
-  </svg>`;
+const generateSvgTemplate = (pathData, width, height, size) => `<svg
+   xmlns="http://www.w3.org/2000/svg"
+   width="{${size}}em"
+   height="{${size}}em"
+   viewBox="0 0 ${width} ${height}"
+   class="{className}">
+  ${pathData}
+</svg>`;
 
 const generateComponentTemplate = ({
                                        scriptContent, size = 'size', pathData, width, height
@@ -32,13 +32,14 @@ function generateComponent(pathData, height, width, componentName, isTypescript)
     display?: boolean;
     occupy?: boolean;
     size?: number;
+    class?: string;
   }
 </script>
 
 <script lang="ts">
-  const { display = false, occupy = true, size = 0.7 }: ${componentName}Props = $props();
+  const {display = false, occupy = true, size = 0.7, class: className = ''}: ${componentName}Props = $props();
 </script>` : `<script>
-  const { display = false, occupy = true, size = 0.7 } = $props();
+  const {display = false, occupy = true, size = 0.7, class: className = '' } = $props();
 </script>`;
 
     return generateComponentTemplate({
@@ -69,7 +70,8 @@ export async function downloadIcon(collection, name, options = {}) {
         // Fetch and validate icon
         const response = await axios.get(`https://api.iconify.design/${collection}/${name}.svg`);
         if (response.status !== 200) {
-            throw new Error(`Failed to download icon ${collection}:${name}`);
+            console.log(`Failed to download icon ${collection}:${name}`);
+            return [];
         }
 
         // Process icon data
@@ -91,6 +93,7 @@ export async function downloadIcon(collection, name, options = {}) {
 
         return [outputPath];
     } catch (error) {
-        throw new Error(`Error processing icon: ${error.message}`);
+        console.error(`Failed to download icon ${collection}:${name}:\n ${error.message}`);
+        return [];
     }
 }
